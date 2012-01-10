35f50a39a076a3a60b08943eab707ee14e2c0130
pogermano@gmail.com <pogermano@gmail.com>
2012-01-10 16:17:21 -0200

    Added feature to ensure Devise signup is always working

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :test do
   gem 'factory_girl'
 end
 
+  gem 'devise', '~> 1.4.3'
 
 
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Ticketee::Application.routes.draw do
+  devise_for :users
+
   root :to => "projects#index"
   resources :projects do
     resources :tickets
   end
+
+  devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
